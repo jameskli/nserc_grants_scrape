9176978e3c9a3fe8a193c8f5c5db4a72c2906cec
James Li <jameskli@gmail.com>
2020-02-12 13:15:47 -0500

    added requests and lxml to req.txt

--- emina/pare.py
+++ emina/pare.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import csv
 
 INPUT_FILE = 'result_400001_500000.csv'
 CHECK_STRING = 'N/A'
+
+
 def main():
     count = 0
     output_filename = 'pared_'+INPUT_FILE
@@ -14,21 +16,19 @@ def main():
 
         for row in csv_reader:
             if CHECK_STRING in row:
-                count+=1
-                print "Error in Row {}".format(row[0])
+                count += 1
+                print("Error in Row {}".format(row[0]))
                 with open(error_filename, 'a+') as error_file:
-                    #error_csv_writer = csv.writer(error_file, quoting=csv.QUOTE_ALL)
+                    # error_csv_writer = csv.writer(error_file, quoting=csv.QUOTE_ALL)
                     error_file.write(row[0])
                     error_file.write('\n')
             else:
                 with open(output_filename, 'a+') as output_file:
-                    out_csv_writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
+                    out_csv_writer = csv.writer(
+                        output_file, quoting=csv.QUOTE_ALL)
                     out_csv_writer.writerow(row)
-        
-        print "{} Errors found".format(count)
 
-
-
+        print("{} Errors found".format(count))
 
 
 main()
